d2f86fc443a6ee23bb0b3b8eb1ca7a523a3e5192
Vadim Nazarov <vdn@mailbox.org>
2017-03-14 01:02:56 +0300

    update selection with small proportions

--- selection.py
+++ selection.py
@@ -8,60 +8,55 @@ from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
 from keras.callbacks import ModelCheckpoint
 import numpy as np
+from numpy.random import randint
 import random
 import sys
 import re
 import pandas as pd
 import theano
+from scipy import sparse
 
 #
 # How to fight with nans http://stackoverflow.com/questions/37232782/nan-when-training-regression-net-using-keras-and-theano
 #
 
-# After more than 100 iterations here are results for the first 20 receptors:
-#6s - loss: 0.0566
-#[[  5.0744682   11.84071827]
-# [  5.22786273  11.8524065 ]
-# [  5.50443683  11.84071827]
-# [  6.25304755  11.89706039]
-# [  6.33391     11.79355621]
-# [  6.38307337  11.76680374]
-# [  6.37860409  11.83020401]
-# [  6.5170848   11.84064388]
-# [  6.5273809   11.85927868]
-# [  6.60817853  11.79740143]
-# [  6.61521585  11.70992184]
-# [  6.71463049  11.93083572]
-# [  6.94914894  11.79218006]
-# [  6.92190991  11.87408543]
-# [  6.92383114  11.79092789]
-# [  7.0442619   11.84865761]
-# [  7.17130679  11.81984138]
-# [  7.16884676  11.81451893]
-# [  7.17130679  11.85754585]
-# [  7.21923525  11.82061195]]
-#
-# Despite quite low loss, the proportions are looking bad. It seems that the optimizer
-# "narrowed" the proportions in order to minimize loss (I checked - all proporions almost the same). 
-# I'm pretty sure that such narrowing is due to the very heavy tail of receptors with 
-# almost identical proportions (~ 11.8 in neg-log-scale).
-
 VERBOSE=2
-MAX_DATA = 30000
+MAX_DATA = 10000
 
 #theano.config.floatX="float32"
 #theano.config.device="gpu1"
 #theano.config.lib.cnmem="1."
 
+#################
+# Load the data #
+#################
 R_exp_data = pd.read_table("azh.txt")
 R_exp_data = R_exp_data[map(lambda x: not ("*" in x or "~" in x), R_exp_data["CDR3.amino.acid.sequence"])]
-R_exp = R_exp_data["CDR3.amino.acid.sequence"][:MAX_DATA]
-R_exp_prop = R_exp_data["Umi.proportion"][:MAX_DATA]
-print("Experimental:\n -- #sequences:\t", len(R_exp), "\n -- #chars:\t", sum([len(x) for x in R_exp]))
-print(" -- #nans:\t", R_exp.isnull().sum().sum())
+R_exp_data = R_exp_data.loc[R_exp_data["Umi.count"] > 0, :]
+R_exp_seq = R_exp_data["CDR3.amino.acid.sequence"]
+R_exp_prop = R_exp_data["Umi.proportion"]
+print("Experimental:\n -- #sequences:\t", len(R_exp_seq), "\n -- #chars:\t", sum([len(x) for x in R_exp_seq]))
+print(" -- #nans:\t", R_exp_seq.isnull().sum().sum())
+
+# print(sum(R_exp_data["Umi.count"] >= 10)) # 4274
+# print(sum(R_exp_data["Umi.count"] >= 4))  # 16684
+# print(sum(R_exp_data["Umi.count"] == 1))  # 189018
+# print(sum(R_exp_data["Umi.count"] == 0))  # 44349
 
 #R_gen = list(pd.read_table("azh.aa.txt")["Amino.acid.sequence"])
 #print("Generated:\n -- #sequences:\t", len(R_gen), "\n -- #chars:\t", sum([len(x) for x in R_gen]))
+
+
+######################
+# Vectorize the data #
+######################
+def vectorize(seq_vec, prop_vec, max_len, chars):
+    X = np.zeros((len(seq_vec), max_len, len(chars)), dtype=np.bool)
+    y = - np.log(np.array(prop_vec))
+    for i, seq in enumerate(seq_vec):
+        for row, char in enumerate(seq):
+            X[i, row, char_indices[char]] = 1
+    return X, y.reshape(len(seq_vec), 1)
 
 
 chars = ["A", "L", "R", 'K', 'N', 'M', 'D', 'F', 'C', 'P', 'Q', 'S', 'E', 'T', 'G', 'W', 'H', 'Y', 'I', 'V']
@@ -72,14 +67,17 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 
 
 #max_len = max(max([len(x) for x in R_exp]), max([len(x) for x in R_gen]))
-max_len = max([len(x) for x in R_exp])
+max_len = max([len(x) for x in R_exp_seq])
 
-X = np.zeros((len(R_exp), max_len, len(chars)), dtype=np.bool)
-y = - np.log(np.array(R_exp_prop))
-for i, seq in enumerate(R_exp):
-    for row, char in enumerate(seq):
-        X[i, row, char_indices[char]] = 1
+logic_big = R_exp_data["Umi.count"] >= 4
+logic_small = R_exp_data["Umi.count"] < 4
+X_big, y_big = vectorize(R_exp_seq[logic_big], R_exp_prop[logic_big], max_len, chars)
+X_small, y_small = vectorize(R_exp_seq[logic_small], R_exp_prop[logic_small], max_len, chars)
 
+        
+###################
+# Build the model #
+###################
 
 if len(sys.argv) > 1:
     print("Loading model:", sys.argv[1])
@@ -101,26 +99,46 @@ else:
     # Use MSLE instead of MSE because in case of log transformation of our data
     # the model will pay too much attention to optimization of differences for small
     # receptors, which has large negative (= large absolute value) values.
-    model.compile(loss='msle', optimizer=optimizer)
+    model.compile(loss='mse', optimizer=optimizer)
 
 
 print(model.summary())
+
+
+###################
+# Train the model #
+###################
+def generate_batch(max_data):
+    while True:
+        to_sample_big   = int(.8 * max_data)
+        to_sample_small = max_data - to_sample_big
+        indices_big   = randint(0, X_big.shape[0],   size=to_sample_big)
+        indices_small = randint(0, X_small.shape[0], size=to_sample_small)
+        yield np.vstack([X_big[indices_big], X_small[indices_small]]), \
+              np.vstack([y_big[indices_big], y_small[indices_small]])
 
 
 for iteration in range(1, 100):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, 
-              batch_size=128, 
-              nb_epoch=1, 
-              verbose=VERBOSE,
-              callbacks = [ModelCheckpoint(filepath = "model." + str(iteration % 2) + ".{epoch:02d}.hdf5")])
+#     model.fit(X, y, 
+#               batch_size=128, 
+#               nb_epoch=1, 
+#               verbose=VERBOSE,
+#               callbacks = [ModelCheckpoint(filepath = "model." + str(iteration % 2) + ".{epoch:02d}.hdf5")])
+    model.fit_generator(generate_batch(128), 
+                        samples_per_epoch=1280*3, 
+                        nb_epoch=6, 
+                        verbose=VERBOSE, 
+                        callbacks = [ModelCheckpoint(filepath = "model." + str(iteration % 2) + ".{epoch:02d}.hdf5")])
 
-    a = y[:20].reshape((20,1))
-    b = model.predict(X[:20,:,:])
-    print(np.hstack([a, b]))
+    print("\nPredict big proportions:\n\treal\t\tpred")
+    a = y_big[:20].reshape((20,1))
+    b = model.predict(X_big[:20,:,:])
+    print(np.hstack([a, b]), "\n")
     
-    a = y.reshape((30000,1))
-    b = model.predict(X)
+    print("Predict small proportions:\n\treal\t\tpred")
+    a = y_small[:20].reshape((20,1))
+    b = model.predict(X_small[:20,:,:])
     print(np.hstack([a, b]))
