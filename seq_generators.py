326cbb1f21565107cd0a2162f16fd2b10f52249b
Soph <sptolstoukhova@gmail.com>
2017-03-18 14:02:41 +0300

    Update seq_generators.py

--- seq_generators.py
+++ seq_generators.py
@@ -4,10 +4,12 @@ chars = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', '
 ch_begin = ['A', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 ch_end = ['A', 'C', 'D', 'E', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'Y']
 
-max_distance = 3
+max_distance = 2
     
 def close(seq):
-    distance = randint(low=1, high=max_distance)
+    
+    distance = randint(low=1, high=max_distance+1)
+    
     for i in range(distance):
         pos = randint(1, len(seq)-1)
         n = randint(3)
@@ -20,15 +22,21 @@ def close(seq):
             m = randint(len(chars)) 
             new_seq = seq[:pos] + chars[m] + seq[pos+1:]
         seq = new_seq
-    return new_seq
+    if not seq in base_seqs:
+        return new_seq
+    else:
+        return close(seq)
 
-def not_receptors():
-    length = randint(6,23)
-    first = ch_begin[randint(len(ch_begin))]
-    last = ch_end[randint(len(ch_end))]
-    middle = str()
-    middle_ind = random_integers(len(chars)-1, size=length-2)
-    for k in middle_ind:
-        middle += chars[k]
-    seq = first + middle + last
-    return seq
+def not_receptors(n):
+    lst = []
+    for i in range(n):
+        length = randint(6,28)
+        first = ch_begin[randint(len(ch_begin))]
+        last = ch_end[randint(len(ch_end))]
+        middle = str()
+        middle_ind = random_integers(len(chars)-1, size=length-2)
+        for k in middle_ind:
+            middle += chars[k]
+        seq = first + middle + last
+        lst.append(seq)
+    return lst
